14a23df2c774a3c24e1781db20c2d609a911cf40
Luna Cheney <puddalt@gmail.com>
2026-04-25 21:56:47 +1200

    commit

---
+++ test.lua
@@ -0,0 +1,13 @@
+local component = require("component")
+local sides = require("sides")
+local s = (function(l) return l.serialize end)(require("serialization"))
+
+local xnet = component.xnet
+
+for i,b in ipairs(xnet.getConnectedBlocks()) do
+  if(b.name == "tconstruct:smeltery_io") then
+    for i,x in ipairs(xnet.getFluids(b.pos)) do
+      print(s(x))
+    end
+  end
+end
