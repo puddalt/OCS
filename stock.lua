64f8fd284c65738d8c89332a51a4aa04afd735f8
Luna Cheney <puddalt@gmail.com>
2026-04-25 21:31:01 +1200

    commit

--- stock.lua
+++ stock.lua
@@ -7,8 +7,7 @@ local xnet = component.xnet
 for i,b in ipairs(xnet.getConnectedBlocks()) do
   if(b.name == "tconstruct:smeltery_io") then
     for i,x in ipairs(xnet.getFluids(b.pos)) do
-      print(x.content.label)
-      print(x.content.amount)
+      print("-" .. x.content.label .. x.content.amount .. "mb")
     end
   end
 end
