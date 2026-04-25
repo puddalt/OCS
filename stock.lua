5bfae5d4ae6cde989f0ed8482683e1f9fbe385c1
Luna Cheney <puddalt@gmail.com>
2026-04-25 22:04:44 +1200

    commit

--- stock.lua
+++ stock.lua
@@ -1,5 +1,7 @@
 local component = require("component")
 local sides = require("sides")
+local term = require("term")
+local colours = require("colors")
 local s = (function(l) return l.serialize end)(require("serialization"))
 
 local xnet = component.xnet
@@ -7,7 +9,12 @@ local xnet = component.xnet
 for i,b in ipairs(xnet.getConnectedBlocks()) do
   if(b.name == "tconstruct:smeltery_io") then
     for i,x in ipairs(xnet.getFluids(b.pos)) do
-      print("> " .. x.content.name .. " " .. x.content.amount .. "mb")
+      io.write("> " .. x.content.name .. " " )
+      term.setTextColor(colours.blue)
+      term.setBackgroundColor(colours.blue)
+      io.write(x.content.amount .. "mb \n")
+      term.setTextColor(colours.white)
+      term.setBackgroundColor(colours.black)
     end
   end
 end
