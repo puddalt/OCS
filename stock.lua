221849feebaa4c904e47061aa0ee6ab2f3391bfc
Luna Cheney <puddalt@gmail.com>
2026-04-25 21:58:28 +1200

    commit

--- stock.lua
+++ stock.lua
@@ -7,18 +7,7 @@ local xnet = component.xnet
 for i,b in ipairs(xnet.getConnectedBlocks()) do
   if(b.name == "tconstruct:smeltery_io") then
     for i,x in ipairs(xnet.getFluids(b.pos)) do
-      local name = {}
-      for str in x.content.label:gmatch("%S+") do
-        table.insert(name, str)
-      end
-
-      if name[1] == "Molten" then
-        table.remove(name, 1)
-      end
-
-      local name_str = table.concat(name, " ")
-
-      print("> " .. name_str .. " " .. x.content.amount .. "mb")
+      print("> " .. x.content.name .. " " .. x.content.amount .. "mb")
     end
   end
 end
