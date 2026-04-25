5f8f44761f0874d9b8523c94b0a09349a71b2c76
Luna Cheney <puddalt@gmail.com>
2026-04-25 21:22:32 +1200

    changes

--- stock.lua
+++ stock.lua
@@ -3,12 +3,12 @@ local sides = require("sides")
 local s = (function(l) return l.serialize end)(require("serialization"))
 
 local xnet = component.xnet
-local smelterys = {}
 
 for i,b in ipairs(xnet.getConnectedBlocks()) do
   if(b.name == "tconstruct:smeltery_io") then
     for i,x in ipairs(xnet.getFluids(b.pos)) do
       print(x.content.label)
+      print(s(x))
     end
   end
 end
