d8599b4838d28e79b9a6b2be96b4a6ce55d84629
Luna Cheney <puddalt@gmail.com>
2026-04-26 21:16:10 +1200

    commit

--- ssh.lua
+++ ssh.lua
@@ -8,23 +8,47 @@ local timeout = 5
 
 modem.open(port)
 
-print("Enter command to send to remote machine:")
-local command = term.read():gsub("\n", "")
+modem.broadcast(port, "ls -la")
 
-if command ~= "" then
-  print("Sending: " .. command)
-  modem.broadcast(port, command)
+print("--- SNIFFER ACTIVE (Port 400) ---")
+print("Press Ctrl+C to stop.")
+
+while true do
+  -- Pull ANY event, not just modem_message, to see if anything is happening
+  local e = {event.pull(10)} -- 10 second timeout per loop
   
-  print("Waiting for response...")
-  local _, _, sender, p, _, response = event.pull(timeout, "modem_message", nil, nil, port)
-  
-  if response then
-    print("\n--- Remote Output ---")
-    print(response)
-    print("---------------------")
+  if e[1] == "modem_message" then
+    print("\n[PACKET RECEIVED]")
+    print("1. Event Name: " .. tostring(e[1]))
+    print("2. Local Addr: " .. tostring(e[2]):sub(1,8) .. "...")
+    print("3. Remote Addr: " .. tostring(e[3]):sub(1,8) .. "...")
+    print("4. Port:        " .. tostring(e[4]))
+    print("5. Distance:    " .. tostring(e[5]))
+    print("6. Data/Msg:    " .. (tostring(e[6]) or "NIL"))
+  elseif e[1] == nil then
+    print("... still waiting (no packets) ...")
   else
-    print("Error: Request timed out. No response from remote machine.")
+    print("Ignored event: " .. tostring(e[1]))
   end
-else
-  print("Cancelled.")
 end
+
+-- print("Enter command to send to remote machine:")
+-- local command = term.read():gsub("\n", "")
+
+-- if command ~= "" then
+--   print("Sending: " .. command)
+--   modem.broadcast(port, command)
+  
+--   print("Waiting for response...")
+--   local _, _, sender, p, _, response = event.pull(timeout, "modem_message", nil, nil, port)
+  
+--   if response then
+--     print("\n--- Remote Output ---")
+--     print(response)
+--     print("---------------------")
+--   else
+--     print("Error: Request timed out. No response from remote machine.")
+--   end
+-- else
+--   print("Cancelled.")
+-- end
